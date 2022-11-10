7efa2592865603c7d309ace0b24bff9f274f26ca
Orion Grieco <Orion.grieco@Gmail.com>
2022-11-10 14:33:50 -0500

    Update calculator.asm

--- Week11/src/calculator.asm
+++ Week11/src/calculator.asm
@@ -2,8 +2,9 @@
 
 prompt1:    .asciiz      "Enter the first number: "
 prompt2:    .asciiz      "Enter the second number: "
-result:     .asciiz      "Final result of add is: "
-
+result1:     .asciiz      "Final result of add is: "
+result2:     .asciiz      "Final result of subtract is: "
+nl: .asciiz "\n"
 .text
 .globl main
   main:
@@ -28,11 +29,11 @@ result:     .asciiz      "Final result of add is: "
   syscall
   move $t1, $v0
 
-   add $t2, $t0, $t1    # add $t0 and $t1 and to $t7
-
+   add $t2, $t0, $t1    # add $t0 and $t1 and to $t2
+   sub $t3, $t0, $t1    # subtract $t1 from $t0 and add to $t3
    # print the result message
    li $v0, 4
-   la $a0, result
+   la $a0, result1
    syscall
 
    # print the result
@@ -40,5 +41,19 @@ result:     .asciiz      "Final result of add is: "
    la $a0, ($t2)
    syscall
    
-   li $v0, 10
-	 syscall
+   
+   # print the result message
+   li $v0, 4
+   la $a0, nl
+   syscall
+   
+   # print the result message
+   li $v0, 4
+   la $a0, result2
+   syscall
+
+   # print the result
+   li $v0,1
+   la $a0, ($t3)
+   syscall
+   
